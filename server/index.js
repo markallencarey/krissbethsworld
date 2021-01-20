871305e0885180f7ebf136e9132e1546be7ea80c
Mark Carey <markallencarey@gmail.com>
2021-01-20 00:24:01 -0700

    KBproducts 01.19.21-03

--- server/index.js
+++ server/index.js
@@ -40,10 +40,10 @@ app.delete('/api/cart/clear', cartCtrl.clearCart)
 //stripeController
 app.post('/create-checkout-session', stripeCtrl.createCheckoutSession)
 
-app.use(express.static(__dirname + '/../build'))
+app.use(express.static(`${__dirname}/../build`))
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '../build/index.html'))
+  res.sendFile(path.join(__dirname, '../build/index.html'))
 })
 
 massive({
